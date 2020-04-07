dc77d86321ac31fa9ca588835dca2d3bef15ac3b
AugustoLatge <augustoslatge@gmail.com>
2020-04-07 19:12:31 -0400

    end of routing module

--- burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,29 +92,17 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert("You continued!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Aragorn II Elessar",
-        address: {
-          street: "Palace Street",
-          zipCode: "10000",
-          country: "Gondor"
-        },
-        email: "test@test.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    axios
-      .post("/orders.json", order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
+    }
+    queryParams.push("price=" + this.state.totalPrice);
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      // search: "?ingredients=" + JSON.stringify(this.state.ingredients)
+      search: "?" + queryString
+    });
   };
 
   render() {
